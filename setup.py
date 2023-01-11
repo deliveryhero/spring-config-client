9f5955e2229b54eb57e7b43fbcd0ef80731fdb5b
Bayram Kaya <bayram.kaya@yemeksepeti.com>
2023-01-11 16:34:50 +0300

    Bump version: 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
